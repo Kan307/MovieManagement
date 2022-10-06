a529dff0bfc42f473ae0f3cadf2011779e335c3f
kan307 <binikobiniko@gmail.com>
2022-10-06 13:18:17 +0900

    プロジェクト ファイルを追加します。

---
+++ MovieManagement/Data/MovieManagementContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MovieManagement.Models;
+
+namespace MovieManagement.Data
+{
+    public class MovieManagementContext : DbContext
+    {
+        public MovieManagementContext (DbContextOptions<MovieManagementContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MovieManagement.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ MovieManagement/Models/Movie.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieManagement.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+
+        public DateTime ReleaseDate { get; set; }
+
+        public string Genre { get; set; }
+
+        public Decimal Price { get; set; }
+
+
+
+
+
+
+
+    }
+}
